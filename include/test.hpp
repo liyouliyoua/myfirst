e67ccca458ec0a16a19270d9a1c2dc1b6c4e9748
LiYou <You.Li5@zeekrlife.com>
2022-09-13 21:31:54 +0800

    modified by liyou

---
+++ include/test.hpp
@@ -0,0 +1,8 @@
+include <iostream>
+
+typedef strut{
+	std::int a;
+	std::string b
+
+
+}test_strut
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+include <iostream>
+
+int main(int argc, int **argv)
+{
+	std::cout<<"hello123 "<<std::endl;
+
+	return 0;	
+
+	
+
+}
